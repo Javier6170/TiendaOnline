45f6dd8ce4dd02f5e5545b165213aa8f6a1372cc
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-10-07 23:12:18 -0500

    first commit

---
+++ src/main/kotlin/eam/edu/ingesoft/onlinestore/repositories/StoreRepository.kt
@@ -0,0 +1,43 @@
+package eam.edu.ingesoft.onlinestore.repositories
+
+import eam.edu.ingesoft.onlinestore.model.Product
+import eam.edu.ingesoft.onlinestore.model.ProductStore
+import eam.edu.ingesoft.onlinestore.model.Store
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+
+@Component
+@Transactional
+class StoreRepository {
+    @Autowired
+    lateinit var em: EntityManager
+
+    fun create(store: Store){
+        em.persist(store)
+    }
+
+
+    fun find(id: String): Store?{
+        return em.find(Store::class.java, id)
+    }
+
+    fun update(store: Store) {
+        em.merge(store)
+    }
+
+    fun delete(id: String) {
+        val store = find(id)
+        if (store!=null) {
+            em.remove(store)
+        }
+    }
+
+    fun findByCity(id: Long): List<ProductStore> {
+        val query = em.createQuery("SELECT store FROM Store store WHERE store.city.id =: id")
+        query.setParameter("id", id)
+
+        return query.resultList as List<ProductStore>
+    }
+}
